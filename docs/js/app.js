ea3649fa32ae0e5fb2d51a04412fd6d0e79efbb6
William <76045218+silentcodear@users.noreply.github.com>
2021-10-04 15:17:11 +0100

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -68,8 +68,8 @@ App = {
     var loader  = $('#loader');
     var content = $('#content');
 
-    loader.show();
-    content.hide();
+    //loader.show();
+    //content.hide();
 
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
