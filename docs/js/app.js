da9da490a4f563633ef2281d6ff4178dae123aae
William <76045218+silentcodear@users.noreply.github.com>
2021-10-04 10:29:40 +0100

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -11,8 +11,8 @@ App = {
   account: '0x0',
   loading: false,
   tokenPrice: 1000000000000000,
-  tokensSold: 0,
-  tokensAvailable: 750000,
+  tokensSold: 1000,
+  tokensAvailable: 899000,
 
   init: function() {
     console.log("App initialized...")
